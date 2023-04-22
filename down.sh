36912bb8f8ebc94f6a15f490cf67b5b570d0481c
pseudoclone <prince.077bei27@acem.edu.np>
2023-04-22 15:09:40 +0545

    add: Instruction file

--- down.sh
+++ down.sh
@@ -2,25 +2,25 @@
 
 user=$1
 
-export link=$(curl https://proxitok.pufe.org/@$user | grep "warning" | cut -d'=' -f3 | cut -d'"' -f2)
+export link=$(curl https://proxitok-instance-put-here.com/@$user | grep "warning" | cut -d'=' -f3 | cut -d'"' -f2)
 
 function iterate(){
 
 
-	tee ~/pur/nitro/file1.txt <<<"$link"
+	tee downRep/file1.txt <<<"$link"
 
 
-	for i in $(cat ~/pur/nitro/file1.txt)
+	for i in $(cat downRep/file1.txt)
 	do
 		yt-dlp -o "~/pur/nitro/%(title)s.%(ext)s" -fb "$i"
 	done
 	rm -rf ~/pur/nitro/file1.txt
 
-	curl https://proxitok.pufe.org/@$user | grep "cursor"
+	curl https://proxitok-instance-put-here.com/@$user | grep "cursor"
 	if [ $? -eq 0 ]; then
 
-		cursor_id=$(curl https://proxitok.pufe.org/@$user | grep "cursor" | cut -d'?' -f2 | cut -d'=' -f2 | cut -d'"' -f1)
-		export link=$(curl https://proxitok.pufe.org/@$user?cursor=$cursor_id | grep "warning" | cut -d'=' -f3 | cut -d'"' -f2)
+		cursor_id=$(curl https://proxitok-instance-put-here.com/@$user | grep "cursor" | cut -d'?' -f2 | cut -d'=' -f2 | cut -d'"' -f1)
+		export link=$(curl https://proxitok-instance-put-here.com/@$user?cursor=$cursor_id | grep "warning" | cut -d'=' -f3 | cut -d'"' -f2)
 		iterate
 	fi
 	exit
