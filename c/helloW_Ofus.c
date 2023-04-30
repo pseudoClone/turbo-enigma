5a8f8ae28009d880b367a28d3fd0b21da89bc54f
pseudoclone <prince.077bei027@acem.edu.np>
2023-04-30 13:15:35 +0545

    add: jump into code ofuscation, inspired by Bellard

---
+++ c/helloW_Ofus.c
@@ -0,0 +1,23 @@
+#define pp_ {
+#define pp__p }
+#define _p =
+#define _p_ char
+#define lk (
+#define klk )
+#define p_ printf
+??= define \
+_p__ \
+main
+_p__ lk klk pp_ _p_
+_x ??( 0xA ??)\
+_p\
+"a\
+bcdefghijklm\
+nopqr\
+stuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123\
+4567890";
+#define \
+__p\
+typedef;
+p_ lk"Hello world!??/n"klk;
+pp__p
