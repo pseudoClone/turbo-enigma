909ea4662f0372b915112199421432f28c4f09ba
pseudoClone <prince.077bei027@acem.edu.np>
2025-07-02 10:21:04 +0545

    feat(C): Test C header file compatibility in ESP-IDF

---
+++ ESP32C3/bigDumpofCode/main/bigDumpofCode.c
@@ -0,0 +1,10 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+#include "include/init.h"
+
+void app_main(void)
+{
+        doPrint();
+        printf("App starting.....\n");
+}
